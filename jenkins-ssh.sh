f8e564af4aee7055696b6508a731b9f6f050353f
sa-mh <62239021+sa-mh@users.noreply.github.com>
2020-11-20 10:53:20 +0000

    Update jenkins-ssh.sh

--- jenkins-ssh.sh
+++ jenkins-ssh.sh
@@ -3,6 +3,7 @@
 ssh -i /home/jenkins/.ssh/id_rsa ubuntu@54.72.173.119 << EOF
 git clone https://github.com/sa-mh/Project-Flask-App
 cp run-tests.sh Project-Flask-App
+cp docker-compose.yaml Project-Flask-App
 cd Project-Flask-App
 ls
 bash run-tests.sh
