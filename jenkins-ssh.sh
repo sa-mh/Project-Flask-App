e38b4d61fd1d39bd270ffb8a0336885392943fb5
sa-mh <62239021+sa-mh@users.noreply.github.com>
2020-11-20 10:30:03 +0000

    Update jenkins-ssh.sh

--- jenkins-ssh.sh
+++ jenkins-ssh.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ssh -i /home/jenkins/.ssh/id_rsa ubuntu@54.72.173.119 << EOF
-
+git clone https://github.com/sa-mh/Project-Flask-App
+cd Project-Flask-App
 bash run-tests.sh
-
+cd ..
+sudi rmi -r Project-Flask-App
 EOF
