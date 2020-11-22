2e43cfeb4210f75d022579dba75ac59811a81fcc
sa-mh <62239021+sa-mh@users.noreply.github.com>
2020-11-22 16:00:48 +0000

    Update jenkins-ssh.sh

--- jenkins-ssh.sh
+++ jenkins-ssh.sh
@@ -5,6 +5,14 @@ git clone https://github.com/sa-mh/Project-Flask-App
 cp run-tests.sh Project-Flask-App
 cp docker-compose.yaml Project-Flask-App
 cd Project-Flask-App
+sudo docker login -u horners -p ProjectPassword
+cd frontend
+sudo docker build -t horners/frontend:latest .
+cd ../backend
+sudo docker build -t horners/backend:latest .
+sudo docker push horners/backend:latest
+sudo docker push horners/frontend:latest
+cd ..
 ls
 bash run-tests.sh
 cd ..
